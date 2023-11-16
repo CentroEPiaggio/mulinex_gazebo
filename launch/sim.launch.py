fe738c0b7bde27e1542d85342d00e0ccbb3c9249
Simone Tolomei <tolomei.simone5@gmail.com>
2023-11-16 09:28:26 +0000

    changed launch structure

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -2,60 +2,85 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, Command
+from launch.substitutions import LaunchConfiguration, Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
-  use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-  urdf = os.path.join(
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    urdf = os.path.join(
         get_package_share_directory('mulinex_description'),
         'urdf',
         'mulinex.xacro'
-  )
-  print("urdf_file_name : {}".format(urdf))
+    )
+
+    print("urdf_file_name : {}".format(urdf))
+
+    gazebo = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [PathJoinSubstitution([FindPackageShare("gazebo_ros"), "launch", "gazebo.launch.py"])]
+            ),
+            launch_arguments={"verbose": "false"}.items(),
+    )
+
+    # Get URDF via xacro
+    mulinex_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ", urdf, " use_gazebo:=true ",
+        ]
+    )
+
+    robot_description = {"robot_description": mulinex_description_content}
 
 
-  return LaunchDescription([
+    node_robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description],
+    )
+
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-topic", "robot_description", "-entity", "mulinex",
+                   "-x", "0", "-y", "0", "-z", "0.5"],
+        output="screen",
+    )
+
+    control_manager_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        name = 'controller_manager',
+        parameters=[robot_description],
+        output="screen",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        #arguments=['GazeboSystem'],
+        arguments=["forward_position_controller", "--controller-manager", "/controller_manager"],
+    )
+
+
+    return LaunchDescription([
 
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value='false',
+            default_value='true',
             description='Use simulation (Gazebo) clock if true'),
-
-        ExecuteProcess(
-            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
-            output='screen'),
-
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time},
-            {'robot_description': Command(['xacro', ' ', urdf, ' use_gazebo:=true ',
-                        ' front_hfe:=-2.094', 
-                        ' hind_hfe:=2.094',
-                        ' front_kfe:=0.7854',
-                        ' hind_kfe:=-0.7854',])}],
-            ),
-
-        Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            name='joint_state_publisher',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time}]
-            ),
-
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            name='urdf_spawner',
-            output='screen',
-            arguments=["-topic", "/robot_description", "-entity", "mulinex",
-                        "-x", "0", "-y", "0", "-z", "0.5",]),
 
         IncludeLaunchDescription(
           PythonLaunchDescriptionSource([os.path.join(
@@ -63,18 +88,11 @@ def generate_launch_description():
             'launch',
             'mulinex_inference.launch.py')])
         ),
+        gazebo,
+        control_manager_node,
+        node_robot_state_publisher,
+        spawn_entity,
+        joint_state_broadcaster_spawner,
+        robot_controller_spawner,
 
-        Node(
-            package='controller_manager',
-            executable='ros2_control_node',
-            parameters=[robot_description, 
-                        os.path.join(
-                            get_package_share_directory('mulinex_gazebo'),
-                            'config',
-                            'my_robot_controllers.yaml')],
-            output={
-                'stdout': 'screen',
-                'stderr': 'screen',
-            },
-        ),
   ])
